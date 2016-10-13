dd8c198e340cb075c2964c518b349c974b881d45
Mandeep1009 <yadavmandeep4@gmail.com>
2016-10-13 23:08:30 +0530

    Add files via upload

---
+++ PrintX.java
@@ -0,0 +1,21 @@
+//Write a program to print the “X” pattern:
+
+class PrintX
+{
+	public static void main(String[] args)
+	{
+		for (int i = 1; i <= 5; i++)
+        	{
+            		for (int j = 1; j <= 5; j++)
+            		{
+                		if((i == j) || (i + j == 6)) 
+					{
+                    			System.out.print("* ");
+                    			continue;
+                			}
+                	System.out.print("  ");
+            		}
+            	System.out.println();
+        	}
+	}
+}
